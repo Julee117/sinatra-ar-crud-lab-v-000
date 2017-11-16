61266fe3f59629580300f5c2ed31daadb6f61aa4
Julianne Kim <juliannne@gmail.com>
2017-11-16 16:37:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,9 +17,9 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/:id' do
-    # @post = Post.find_by_id(params[:id])
+    @post = Post.find_by_id(params[:id])
     # OR
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
     erb :show
   end
 
